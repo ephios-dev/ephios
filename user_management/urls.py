6a66af06dba863c04d1dfadc75f26e7e8b617c99
Julian B <jeriox@users.noreply.github.com>
2020-08-28 15:40:34 +0200

    add suggestions

--- user_management/urls.py
+++ user_management/urls.py
@@ -4,5 +4,5 @@ from user_management import views
 
 app_name = "user_management"
 urlpatterns = [
-    path("profile", views.ProfileView.as_view(), name="profile"),
+    path("profile/", views.ProfileView.as_view(), name="profile"),
 ]
