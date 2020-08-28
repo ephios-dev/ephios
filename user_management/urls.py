fe5c19b27b65dfdfaebbff3352b0966889883f1c
gritfessel <grit.fessel@student.hpi.de>
2020-08-28 22:31:08 +0200

    Add UserProfileListView

---
+++ user_management/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from user_management import views
+
+app_name = "user_management"
+urlpatterns = [
+    path("users/", views.UserProfileListView.as_view(), name="user_list", ),
+]
--- user_management/views.py
+++ user_management/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from user_management.models import UserProfile
+
+
+class UserProfileListView(ListView):
+    model = UserProfile
+
+    def get_queryset(self):
+        return UserProfile.objects.all()
