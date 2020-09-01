aa9984bee5e031c9c96b54e4db20b6afeeb4d457
Julian B <jeriox@users.noreply.github.com>
2020-09-01 22:50:48 +0200

    add tests that are not working

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,8 +8,36 @@ from user_management.models import UserProfile
 
 
 @pytest.fixture
-def volunteer_user(db):
+def volunteer_group(db):
     volunteer_group = Group.objects.create(name="Volunteers")
+    return volunteer_group
+
+
+@pytest.fixture
+def planner_group(db, volunteer_group):
+    planner_group = Group.objects.create(name="Planner")
+    assign_perm("publish_event_for_group", planner_group, volunteer_user.groups.first())
+    assign_perm("event_management.add_event", planner_group)
+    assign_perm("event_management.delete_event", planner_group)
+    return planner_group
+
+
+@pytest.fixture
+def manager_group(db):
+    manager_group = Group.objects.create(name="Managers")
+    assign_perm("user_management.view_userprofile", manager_group)
+    assign_perm("user_management.add_userprofile", manager_group)
+    assign_perm("user_management.change_userprofile", manager_group)
+    assign_perm("user_management.delete_userprofile", manager_group)
+    assign_perm("auth.view_group", manager_group)
+    assign_perm("auth.add_group", manager_group)
+    assign_perm("auth.change_group", manager_group)
+    assign_perm("auth.delete_group", manager_group)
+    return manager_group
+
+
+@pytest.fixture
+def volunteer_user(db, volunteer_group):
     user = UserProfile.objects.create(
         email="volunteer@test",
         first_name="Volunteer",
@@ -21,31 +49,18 @@ def volunteer_user(db):
 
 
 @pytest.fixture
-def planner_user(db, volunteer_user):
-    planner_group = Group.objects.create(name="Planner")
+def planner_user(db, planner_group):
     user = UserProfile.objects.create(
         email="planner@test", first_name="Planner", last_name="User", date_of_birth=datetime.now()
     )
     user.groups.add(planner_group)
-    assign_perm("publish_event_for_group", planner_group, volunteer_user.groups.first())
-    assign_perm("event_management.add_event", planner_group)
-    assign_perm("event_management.delete_event", planner_group)
     return user
 
 
 @pytest.fixture
-def manager_user(db):
-    manager_group = Group.objects.create(name="Managers")
+def manager_user(db, manager_group):
     user = UserProfile.objects.create(
         email="manager@test", first_name="Manager", last_name="User", date_of_birth=datetime.now()
     )
     user.groups.add(manager_group)
-    assign_perm("user_management.view_userprofile", manager_group)
-    assign_perm("user_management.add_userprofile", manager_group)
-    assign_perm("user_management.change_userprofile", manager_group)
-    assign_perm("user_management.delete_userprofile", manager_group)
-    assign_perm("auth.view_group", manager_group)
-    assign_perm("auth.add_group", manager_group)
-    assign_perm("auth.change_group", manager_group)
-    assign_perm("auth.delete_group", manager_group)
     return user
--- tests/user_management/test_views_group.py
+++ tests/user_management/test_views_group.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 import pytest
+from django.contrib.auth.models import Group
 from django.urls import reverse
 from django_webtest import DjangoTestApp
 from guardian.shortcuts import assign_perm
@@ -12,10 +13,40 @@ from user_management.models import UserProfile
 @pytest.mark.django_db
 class TestGroupView:
     def test_group_list_permission_required(self, django_app, volunteer_user):
-        response = django_app.get(reverse("user_management:group_list"), user=volunteer_user)
-        assert response.status_code == 302
+        response = django_app.get(
+            reverse("user_management:group_list"), user=volunteer_user, status=403
+        )
+        assert response.status_code == 403
 
-    def test_group_list(self, django_app: DjangoTestApp, manager_user):
+    def test_group_list(self, django_app, manager_user):
         response = django_app.get(reverse("user_management:group_list"), user=manager_user)
         assert response.status_code == 200
         assert response.html.findAll(text="Managers")
+
+    # def test_group_edit_button(self, django_app, manager_user):
+    #     response = django_app.get(reverse("user_management:group_list"), user=manager_user)
+    #     response.showbrowser()
+    #     target_response = response.clickbutton(description="Bearbeiten", verbose=True)
+    #     assertRedirects(
+    #         target_response,
+    #         reverse("user_management:group_edit", kwargs={"pk": manager_user.groups.first().pk})
+    #     )
+
+    def test_group_create_permission_required(self, django_app, volunteer_user):
+        response = django_app.get(
+            reverse("user_management:group_add"), user=volunteer_user, status=403
+        )
+        assert response.status_code == 403
+
+    # does not work because of select2 heavy widgets
+    # def test_group_create_successful(self, django_app, manager_user):
+    #     response = django_app.get(reverse("user_management:group_add"), user=manager_user)
+    #     form = response.form
+    #     group_name = "Testgroup"
+    #     form["name"] = group_name
+    #     form["users"].select_multiple(texts=[manager_user.get_full_name()])
+    #     response = form.submit()
+    #     assert response.status == 200
+    #     group = Group.objects.get(name=group_name)
+    #     assert group.name == group_name
+    #     assert group.user_set == UserProfile.objects.filter(pk=manager_user.pk)
