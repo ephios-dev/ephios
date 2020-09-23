bde5a858c0b8a5f9cd9b1a1e4d26e42f17820173
Julian B <jeriox@users.noreply.github.com>
2020-09-23 14:08:04 +0200

    add csp middleware

--- ephios/settings.py
+++ ephios/settings.py
@@ -65,6 +65,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "csp.middleware.CSPMiddleware",
 ]
 
 ROOT_URLCONF = "ephios.urls"
