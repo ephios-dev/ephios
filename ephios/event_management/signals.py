a1df1aaa9da30765355ef93a55607d1b58a6283a
Felix Rindt <felix@rindt.me>
2020-09-23 14:49:39 +0200

    Abstract email notifications (#111)
    * add app configs and fix email signal
    
    * refactor
    
    * refactor
    
    * make email notifications for participants more abstract

--- ephios/event_management/signals.py
+++ ephios/event_management/signals.py
@@ -2,12 +2,12 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from ephios.event_management import mail
-from ephios.event_management.models import LocalParticipation
+from ephios.event_management.models import AbstractParticipation
 
 
 @receiver(
     post_save,
-    sender=LocalParticipation,
+    sender=AbstractParticipation,
     dispatch_uid="ephios.event_management.signals.send_participation_state_changed_mail",
 )
 def send_participation_state_changed_mail(sender, instance, **kwargs):
