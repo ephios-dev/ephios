64249463ce19433b156fb1771616c9706adf0672
Felix Rindt <felix@rindt.me>
2020-09-23 11:37:17 +0200

    refactor

--- ephios/event_management/mail.py
+++ ephios/event_management/mail.py
@@ -37,18 +37,20 @@ def new_event(event):
     mail.get_connection().send_messages(messages)
 
 
-def participation_state_changed(sender, **kwargs):
-    instance = kwargs["instance"]
-    if instance.state != AbstractParticipation.States.USER_DECLINED and instance.user.is_active:
+def participation_state_changed(participation: LocalParticipation):
+    if (
+        participation.state != AbstractParticipation.States.USER_DECLINED
+        and participation.user.is_active
+    ):
         messages = []
 
         # send mail to the user that has been changed
         text_content = _(
             "The status for your participation for the shift {shift} has changed. It is now {status}."
-        ).format(shift=instance.shift, status=instance.get_state_display())
+        ).format(shift=participation.shift, status=participation.get_state_display())
         html_content = render_to_string("email_base.html", {"message_text": text_content})
         message = EmailMultiAlternatives(
-            to=[instance.user.email],
+            to=[participation.user.email],
             subject=_("Your participation state changed"),
             body=text_content,
         )
@@ -57,15 +59,15 @@ def participation_state_changed(sender, **kwargs):
 
         # send mail to responsible users
         responsible_persons = get_users_with_perms(
-            instance.shift.event, only_with_perms_in=["change_event"]
+            participation.shift.event, only_with_perms_in=["change_event"]
         ).distinct()
         subject = _("Participation was changed for your event")
         text_content = _(
             "The participation of {user} for the shift {shift} was changed. The status is now {status}"
         ).format(
-            user=instance.user.get_full_name(),
-            shift=instance.shift,
-            status=instance.get_state_display(),
+            user=participation.user.get_full_name(),
+            shift=participation.shift,
+            status=participation.get_state_display(),
         )
         html_content = render_to_string("email_base.html", {"message_text": text_content})
         for user in responsible_persons:
--- ephios/event_management/signals.py
+++ ephios/event_management/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from ephios.event_management.mail import participation_state_changed
+from ephios.event_management import mail
 from ephios.event_management.models import LocalParticipation
 
 
@@ -10,5 +10,5 @@ from ephios.event_management.models import LocalParticipation
     sender=LocalParticipation,
     dispatch_uid="ephios.event_management.signals.send_participation_state_changed_mail",
 )
-def send_participation_state_changed_mail(sender, **kwargs):
-    participation_state_changed(sender, **kwargs)
+def send_participation_state_changed_mail(sender, instance, **kwargs):
+    mail.participation_state_changed(instance)
