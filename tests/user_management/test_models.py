835af3564b664f7654e2ca7ac24d0bd0beb6da90
Felix Rindt <felix@rindt.me>
2020-10-15 19:50:47 +0200

    Fix 170 (#171)
    * fix 170
    
    * optimize imports

---
+++ tests/user_management/test_models.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+
+import pytest
+import pytz
+from django.utils import timezone
+
+from ephios.user_management.models import QualificationGrant
+
+
+@pytest.fixture
+def qualified_volunteer(volunteer, qualifications):
+    tz = pytz.timezone("Europe/Berlin")
+    QualificationGrant.objects.create(
+        user=volunteer,
+        qualification=qualifications.nfs,
+        expires=datetime(2064, 4, 1).astimezone(tz),
+    )
+    QualificationGrant.objects.create(
+        user=volunteer, qualification=qualifications.c, expires=datetime(2090, 4, 1).astimezone(tz)
+    )
+    QualificationGrant.objects.create(user=volunteer, qualification=qualifications.b, expires=None)
+    return volunteer
+
+
+@pytest.mark.django_db
+def test_user_qualifications(qualified_volunteer, qualifications):
+    tz = pytz.timezone("Europe/Berlin")
+    assert timezone.now().year < 2064  # update tests
+    assert set(qualified_volunteer.qualifications) == {
+        qualifications.nfs,
+        qualifications.c,
+        qualifications.b,
+    }
+    assert qualified_volunteer.qualifications.get(id=qualifications.c.id).expires == datetime(
+        2090, 4, 1
+    ).astimezone(tz)
+    assert qualified_volunteer.qualifications.get(id=qualifications.b.id).expires is None
+    assert qualified_volunteer.qualifications.get(id=qualifications.nfs.id).expires == datetime(
+        2064, 4, 1
+    ).astimezone(tz)
