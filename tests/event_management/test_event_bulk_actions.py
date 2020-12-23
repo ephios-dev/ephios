84299c8849ddef1e779f4f29762cb2a986661ad8
Julian B <jeriox@users.noreply.github.com>
2020-12-23 15:36:06 +0100

    lint

--- tests/event_management/test_event_bulk_actions.py
+++ tests/event_management/test_event_bulk_actions.py
@@ -9,14 +9,18 @@ class TestEventBulkDelete:
     def test_empty_list(self, django_app, planner, event, groups):
         event_count = Event.objects.count()
         form = django_app.get(reverse("event_management:event_list"), user=planner).form
-        form.action = reverse("event_management:event_bulk_delete")  # webtest cannot read the formaction from button
+        form.action = reverse(
+            "event_management:event_bulk_delete"
+        )  # webtest cannot read the formaction from button
         form.submit(name="delete")
         assert event_count == Event.objects.count()
 
     def test_delete_multiple_events(self, django_app, planner, event, multi_shift_event, groups):
         event_count = Event.objects.count()
         form = django_app.get(reverse("event_management:event_list"), user=planner).form
-        form.action = reverse("event_management:event_bulk_delete")  # webtest cannot read the formaction from button
+        form.action = reverse(
+            "event_management:event_bulk_delete"
+        )  # webtest cannot read the formaction from button
         form["bulk_action"] = [event.pk, multi_shift_event.pk]
         confirm_page = form.submit(name="delete")
         # assert confirm_page.html.findAll(string=[event.title, multi_shift_event])
