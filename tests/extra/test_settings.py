e8a3687efcca1cf60925948f19af22c35684dca1
Julian B <jeriox@users.noreply.github.com>
2021-01-02 18:35:25 +0100

    fix test error

--- tests/extra/test_settings.py
+++ tests/extra/test_settings.py
@@ -8,5 +8,4 @@ class TestSettings:
     @override_settings(COMPRESS_ENABLED=True)
     def test_compression(self, django_app, volunteer):
         response = django_app.get(reverse("event_management:index"), user=volunteer)
-        response.showbrowser()
         assert response.status_code == 200
