3dc42a66d6ddb6d8fb8f93b640cf65a39b906c2f
Julian B <jeriox@users.noreply.github.com>
2021-01-03 18:33:54 +0100

    remove bootstrap4 django-bootstrap4 and add v5

--- ephios/settings.py
+++ ephios/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "bootstrap4",
+    "bootstrap5",
     "guardian",
     "django_select2",
     "jquery",
