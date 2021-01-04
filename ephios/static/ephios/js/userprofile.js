b0fbcd5f0786d68a1a796f2925f0436460de256a
Julian B <jeriox@users.noreply.github.com>
2021-01-04 23:28:25 +0100

    fix broken calendar copy link on profile view(#219)

--- ephios/static/ephios/js/userprofile.js
+++ ephios/static/ephios/js/userprofile.js
@@ -1,9 +1,11 @@
-const clipboard = new ClipboardJS('.clipboard-button');
+$(document).ready(function () {
+    const clipboard = new ClipboardJS(".clipboard-button");
 
-clipboard.on("success", function (e) {
-    const btn = $("#calendar-copy-btn");
-    btn.tooltip("show");
-    setTimeout(function () {
-        btn.tooltip("hide")
-    }, 1000);
+    clipboard.on("success", function (e) {
+        const btn = $("#calendar-copy-btn");
+        btn.tooltip("show");
+        setTimeout(function () {
+            btn.tooltip("hide")
+        }, 1000);
+    });
 });
