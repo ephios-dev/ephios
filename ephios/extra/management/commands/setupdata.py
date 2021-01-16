97dda52f711e2040bc2e8947da69181eb59e46d8
Julian Baumann <jeriox@users.noreply.github.com>
2021-01-16 17:12:16 +0100

    add DLRG qualifications (#249)

--- ephios/extra/management/commands/setupdata.py
+++ ephios/extra/management/commands/setupdata.py
@@ -132,7 +132,7 @@ class QualificationDataset(AbstractDataset):
     action = "Create qualification objects for medical qualifications and driver licenses"
 
     def create_objects(self, *args, **options):
-        # pylint: disable=too-many-locals,unused-variable
+        # pylint: disable=too-many-locals,unused-variable,too-many-statements
         medical_category = QualificationCategory.objects.create(
             title=_("Medical"),
             uuid=uuid.UUID("50380292-b9c9-4711-b70d-8e03e2784cfb"),
@@ -145,6 +145,22 @@ class QualificationDataset(AbstractDataset):
             uuid=uuid.UUID("8bdb343e-d78d-4cc9-80e7-7ccc36b556e1"),
         )
 
+        sana = Qualification.objects.create(
+            category=medical_category,
+            title="Sanitäter A",
+            abbreviation="San A",
+            uuid=uuid.UUID("cdb595e8-1dcb-46d2-8013-b915f818b321"),
+        )
+        sana.included_qualifications.add(eh)
+
+        sanb = Qualification.objects.create(
+            category=medical_category,
+            title="Sanitäter B",
+            abbreviation="San B",
+            uuid=uuid.UUID("3fc86c78-2b87-49f7-8721-af7f8d51747c"),
+        )
+        sanb.included_qualifications.add(sana)
+
         sanh = Qualification.objects.create(
             category=medical_category,
             title="Sanitätshelfer",
@@ -152,6 +168,7 @@ class QualificationDataset(AbstractDataset):
             uuid=uuid.UUID("b1faab38-2e7c-4507-b753-06d1e653412d"),
         )
         sanh.included_qualifications.add(eh)
+        sanh.included_qualifications.add(sanb)
 
         rh = Qualification.objects.create(
             category=medical_category,
@@ -164,7 +181,7 @@ class QualificationDataset(AbstractDataset):
         rs = Qualification.objects.create(
             category=medical_category,
             title="Rettungssanitäter",
-            abbreviation="RS",
+            abbreviation="RettSan",
             uuid=uuid.UUID("0b41fac6-ca9e-4b8a-82c5-849412187351"),
         )
         rs.included_qualifications.add(rh)
@@ -326,6 +343,140 @@ class QualificationDataset(AbstractDataset):
         )
         de.included_qualifications.add(d)
         de.included_qualifications.add(d1e)
+
+
+@register_dataset
+class DLRGQualificationDataset(AbstractDataset):
+    identifier = "qualifications_dlrg"
+    action = "Create qualification objects for water rescue and diving"
+
+    def create_objects(self, *args, **options):
+        # pylint: disable=too-many-locals,unused-variable,too-many-statements
+        diving_category = QualificationCategory.objects.create(
+            title=_("Diving"),
+            uuid=uuid.UUID("6212a425-5cdf-49b0-bcb2-5075862a0c4a"),
+        )
+
+        signal = Qualification.objects.create(
+            category=diving_category,
+            title="Signalmann",
+            abbreviation="Signalmann",
+            uuid=uuid.UUID("436aaab0-5e8f-43aa-8f33-7d9ea68f7973"),
+        )
+
+        taucher_1 = Qualification.objects.create(
+            category=diving_category,
+            title="Einsatztaucher Stufe 1",
+            abbreviation="ET1",
+            uuid=uuid.UUID("52fc6a88-6e56-4af5-8021-b8a6412f405c"),
+        )
+        taucher_1.included_qualifications.add(signal)
+
+        taucher_2 = Qualification.objects.create(
+            category=diving_category,
+            title="Einsatztaucher Stufe 2",
+            abbreviation="ET2",
+            uuid=uuid.UUID("481fc9fb-e90a-4b83-8a07-456034aa0a33"),
+        )
+        taucher_2.included_qualifications.add(taucher_1)
+
+        tauchf = Qualification.objects.create(
+            category=diving_category,
+            title="Taucheinsatzführer",
+            abbreviation="TEF",
+            uuid=uuid.UUID("a3c5a3a0-f577-4bf5-8b2f-84108022d793"),
+        )
+        tauchf.included_qualifications.add(taucher_2)
+
+        boats_category = QualificationCategory.objects.create(
+            title=_("Boats (DLRG)"),
+            uuid=uuid.UUID("f1e2619b-56ee-4e4a-b429-b923f8586771"),
+        )
+        Qualification.objects.create(
+            category=boats_category,
+            title="DLRG-Bootsführerschein A",
+            abbreviation="BF Binnen",
+            uuid=uuid.UUID("7ec0c9e2-f211-4370-b796-e309f3e9b448"),
+        )
+        Qualification.objects.create(
+            category=boats_category,
+            title="DLRG-Bootsführerschein B",
+            abbreviation="BF See",
+            uuid=uuid.UUID("1ba25ee1-fa88-4d44-9416-3a024caa6daf"),
+        )
+
+        drsa_category = QualificationCategory.objects.create(
+            title=_("DRSA"),
+            uuid=uuid.UUID("cd10e68f-41fe-4ca0-a624-3ab3eb85bd08"),
+        )
+        rettschw_bronze = Qualification.objects.create(
+            category=drsa_category,
+            title="Rettungsschwimmer Bronze",
+            abbreviation="RS Bronze",
+            uuid=uuid.UUID("247fab6a-8784-4976-a406-985fe47dc683"),
+        )
+        rettschw_silber = Qualification.objects.create(
+            category=drsa_category,
+            title="Rettungsschwimmer Silber",
+            abbreviation="RS Silber",
+            uuid=uuid.UUID("ef95a854-2eeb-431c-a795-bc291b341d49"),
+        )
+        rettschw_silber.included_qualifications.add(rettschw_bronze)
+        rettschw_gold = Qualification.objects.create(
+            category=drsa_category,
+            title="Rettungsschwimmer Gold",
+            abbreviation="RS Gold",
+            uuid=uuid.UUID("b601a18b-cee8-4037-af33-dd7aabeac295"),
+        )
+        rettschw_gold.included_qualifications.add(rettschw_silber)
+
+        wr_category = QualificationCategory.objects.create(
+            title=_("Water rescue (DLRG)"),
+            uuid=uuid.UUID("93574a61-1a6e-4dd5-8df3-40e8c51b693f"),
+        )
+        wr = Qualification.objects.create(
+            category=wr_category,
+            title="Fachausbildung Wasserrettungsdienst (DLRG)",
+            abbreviation="FA WRD",
+            uuid=uuid.UUID("bd7ca398-ed2a-4e97-b681-bc8fb1138ada"),
+        )
+        sr1 = Qualification.objects.create(
+            category=wr_category,
+            title="Strömungsretter I (DLRG)",
+            abbreviation="Strömungsretter I",
+            uuid=uuid.UUID("e1c1fac1-146d-4a52-935e-36455bcf0f87"),
+        )
+        sr1.included_qualifications.add(wr)
+        sr2 = Qualification.objects.create(
+            category=wr_category,
+            title="Strömungsretter II (DLRG)",
+            abbreviation="Strömungsretter II",
+            uuid=uuid.UUID("9c506e7e-a456-4f22-8027-5365ab7dc58c"),
+        )
+        sr2.included_qualifications.add(sr1)
+
+        radio_category = QualificationCategory.objects.create(
+            title=_("Radio"),
+            uuid=uuid.UUID("368fb612-f8c5-476a-a4e7-1478996a8ab3"),
+        )
+        Qualification.objects.create(
+            category=radio_category,
+            title="BOS-Sprechfunker -digital-",
+            abbreviation="Digitalfunk",
+            uuid=uuid.UUID("506c4d4c-df11-4d41-95d7-c4ddfc78b706"),
+        )
+        Qualification.objects.create(
+            category=radio_category,
+            title="BOS-Sprechfunker -analog-",
+            abbreviation="Analogfunk",
+            uuid=uuid.UUID("4974ea66-2086-4987-958b-503de21a285a"),
+        )
+        Qualification.objects.create(
+            category=radio_category,
+            title="DLRG-Sprechfunker",
+            abbreviation="DLRG-Sprechfunker",
+            uuid=uuid.UUID("e8ff96da-0dd3-4664-9b9d-3c4abc1f40de"),
+        )
 
 
 class Command(BaseCommand):
---
+++ tests/user_management/test_setupdata.py
@@ -0,0 +1,16 @@
+import pytest
+from django.core.management import call_command
+
+from ephios.user_management.models import Qualification
+
+
+@pytest.mark.django_db
+def test_qualifications(django_app):
+    call_command("setupdata", "qualifications")
+    assert Qualification.objects.get(title="Ersthelfer")
+
+
+@pytest.mark.django_db
+def test_qualifications(django_app):
+    call_command("setupdata", "qualifications_dlrg")
+    assert Qualification.objects.get(title="Rettungsschwimmer Silber")
