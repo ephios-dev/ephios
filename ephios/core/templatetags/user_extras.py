b835f8bdc529ad7c02f4197cc68651382cdac434
Julian B <jeriox@users.noreply.github.com>
2021-03-02 17:31:26 +0100

    display qualifications on disposition view

--- ephios/core/templatetags/user_extras.py
+++ ephios/core/templatetags/user_extras.py
@@ -1,4 +1,5 @@
 from django import template
+from dynamic_preferences.registries import global_preferences_registry
 
 from ephios.core.consequences import editable_consequences
 
@@ -21,3 +22,12 @@ def workhour_items(user):
 @register.filter(name="render_qualifications")
 def render_qualifications_from_grants(grant_list):
     return ", ".join(map(lambda grant: grant.qualification.abbreviation, grant_list))
+
+
+@register.filter(name="get_relevant_qualifications")
+def get_relevant_qualifications(qualification_queryset):
+    global_preferences = global_preferences_registry.manager()
+    qs = qualification_queryset.filter(
+        category__in=global_preferences["general__relevant_qualification_categories"]
+    )
+    return qs.values_list("abbreviation", flat=True)
