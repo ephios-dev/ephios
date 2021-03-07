5884d92a9b566772615de08e0950ca7a035b830e
Julian B <jeriox@users.noreply.github.com>
2021-03-07 20:08:06 +0100

    refactor

--- ephios/static/ephios/js/main.js
+++ ephios/static/ephios/js/main.js
@@ -33,6 +33,19 @@ $(document).ready(function () {
     });
 })
 
+// Initialize the service worker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/serviceworker.js', {
+        scope: '/'
+    }).then(function (registration) {
+        // Registration was successful
+        console.log('django-pwa: ServiceWorker registration successful with scope: ', registration.scope);
+    }, function (err) {
+        // registration failed :(
+        console.log('django-pwa: ServiceWorker registration failed: ', err);
+    });
+}
+
 function getCookie(name) {
     let cookieValue = null;
     if (document.cookie && document.cookie !== '') {
