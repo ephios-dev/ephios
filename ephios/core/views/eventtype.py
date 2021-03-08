e79fee261797c06fce03e1bdf4b4e5d28dadf18e
Julian Baumann <jeriox@users.noreply.github.com>
2021-03-08 22:11:17 +0100

    refactor permission management in forms (#354)
    Co-authored-by: Felix Rindt <felix@rindt.me>

--- ephios/core/views/eventtype.py
+++ ephios/core/views/eventtype.py
@@ -17,7 +17,7 @@ class EventTypeUpdateView(
     CustomPermissionRequiredMixin, SettingsViewMixin, TemplateView, SingleObjectMixin
 ):
     template_name = "core/eventtype_form.html"
-    permission_required = "core.add_event"
+    permission_required = "core.change_eventtype"
     model = EventType
 
     def dispatch(self, request, *args, **kwargs):
@@ -51,12 +51,13 @@ class EventTypeUpdateView(
 
 
 class EventTypeListView(CustomPermissionRequiredMixin, SettingsViewMixin, ListView):
-    permission_required = "core.add_event"
+    permission_required = "core.view_eventtype"
+    accept_object_perms = False
     model = EventType
 
 
 class EventTypeDeleteView(CustomPermissionRequiredMixin, SettingsViewMixin, DeleteView):
-    permission_required = "core.add_event"
+    permission_required = "core.delete_eventtype"
     model = EventType
 
     def get_success_url(self):
@@ -69,7 +70,8 @@ class EventTypeDeleteView(CustomPermissionRequiredMixin, SettingsViewMixin, Dele
 class EventTypeCreateView(
     CustomPermissionRequiredMixin, SettingsViewMixin, SuccessMessageMixin, CreateView
 ):
-    permission_required = "core.add_event"
+    permission_required = "core.add_eventtype"
+    accept_object_perms = False
     template_name = "core/eventtype_form.html"
     model = EventType
     fields = ["title", "can_grant_qualification"]
--- ephios/extra/permissions.py
+++ ephios/extra/permissions.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import Group, Permission
+from django.forms import BooleanField
 from guardian.ctypes import get_content_type
+from guardian.shortcuts import assign_perm, remove_perm
 from guardian.utils import get_group_obj_perms_model
 
 
@@ -26,3 +28,58 @@ def get_groups_with_perms(obj, only_with_perms_in):
         }
     )
     return Group.objects.filter(**group_filters).distinct()
+
+
+class PermissionField(BooleanField):
+    """
+    This field takes a list of permissions and a permission_target and renders a checkbox that is checked if the target
+    has all given permissions. It requires a permission_target attribute on the form as well as calling the appropriate
+    methods which is taken care of by PermissionFormMixin
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.permission_set = kwargs.pop("permissions")
+        super().__init__(*args, **kwargs)
+
+    def set_initial_value(self, user_or_group):
+        self.target = user_or_group
+        codename_set = set(map(lambda perm: perm.split(".")[-1], self.permission_set))
+        self.initial = codename_set <= set(
+            self.target.permissions.values_list("codename", flat=True)
+        )
+
+    def assign_permissions(self, target):
+        for permission in self.permission_set:
+            assign_perm(permission, target)
+
+    def remove_permissions(self, target):
+        for permission in self.permission_set:
+            remove_perm(permission, target)
+
+
+class PermissionFormMixin:
+    """
+    Mixin for django.forms.ModelForm that handles permission updates for all ephios.extra.permissions.PermissionField on that form
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            if isinstance(field, PermissionField) and self.instance.pk is not None:
+                field.set_initial_value(self.permission_target)
+
+    def save(self, commit=True):
+        target = super().save(commit)
+        to_remove = list()
+        to_assign = list()
+        for key, field in self.fields.items():
+            if isinstance(field, PermissionField):
+                if self.cleaned_data[key]:
+                    to_assign.append(field)
+                else:
+                    to_remove.append(field)
+        for field in to_remove:
+            field.remove_permissions(target)
+        for field in to_assign:
+            field.assign_permissions(target)
+        return target
