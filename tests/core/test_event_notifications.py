f45f53109c6d00aa1696fa56f8d5480256071f5d
Julian B <jeriox@users.noreply.github.com>
2021-03-09 17:15:33 +0100

    fix tests

---
+++ tests/core/test_event_notifications.py
@@ -0,0 +1,63 @@
+import pytest
+from django.core import mail
+from django.urls import reverse
+from guardian.shortcuts import get_users_with_perms
+
+from ephios.core.forms.events import EventNotificationForm
+from ephios.core.models import AbstractParticipation, UserProfile
+
+
+@pytest.mark.django_db
+class TestEventNotifications:
+    def test_mail_new_event(self, django_app, event, volunteer, planner, groups):
+        form = django_app.get(
+            reverse("core:event_notifications", kwargs=dict(pk=event.id)), user=planner
+        ).form
+        form["action"] = EventNotificationForm.NEW_EVENT
+        response = form.submit()
+        assert response.status_code == 302
+        assert (
+            len(mail.outbox)
+            == get_users_with_perms(event, only_with_perms_in=["view_event"]).count()
+        )
+
+    def test_mail_reminder(self, django_app, event, volunteer, planner, groups):
+        form = django_app.get(
+            reverse("core:event_notifications", kwargs=dict(pk=event.id)), user=planner
+        ).form
+        form["action"] = EventNotificationForm.REMINDER
+        response = form.submit()
+        assert response.status_code == 302
+        users_not_participating = UserProfile.objects.exclude(
+            pk__in=AbstractParticipation.objects.filter(shift__event=event).values_list(
+                "localparticipation__user", flat=True
+            )
+        )
+        assert len(mail.outbox) == users_not_participating.count()
+
+    def test_mail_participants(self, django_app, event, volunteer, planner, groups):
+        form = django_app.get(
+            reverse("core:event_notifications", kwargs=dict(pk=event.id)), user=planner
+        ).form
+        form["action"] = EventNotificationForm.PARTICIPANTS
+        form["mail_content"] = "hey there"
+        response = form.submit()
+        assert response.status_code == 302
+        participants = set()
+        for shift in event.shifts.all():
+            participants.update(shift.get_participants())
+        assert len(mail.outbox) == len(participants)
+
+    def test_mail_participants_content_required(
+        self, django_app, event, volunteer, planner, groups
+    ):
+        form = django_app.get(
+            reverse("core:event_notifications", kwargs=dict(pk=event.id)), user=planner
+        ).form
+        form["action"] = EventNotificationForm.PARTICIPANTS
+        response = form.submit()
+        assert response.status_code == 200
+        participants = set()
+        for shift in event.shifts.all():
+            participants.update(shift.get_participants())
+        assert len(mail.outbox) == len(participants)
