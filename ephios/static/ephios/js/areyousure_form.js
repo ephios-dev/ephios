361d2bbfbc61d82118147171cfec35cab3c3d529
Max Garin <maxgarin@gmail.com>
2021-03-11 11:20:08 -0500

    Add listener for inputs that add forms and trigger a rescan

--- ephios/static/ephios/js/areyousure_form.js
+++ ephios/static/ephios/js/areyousure_form.js
@@ -1,3 +1,7 @@
 $(document).ready(function () {
     $('form').areYouSure();
+    // Listener for inputs that can dynamically add new forms and trigger a scan of the new form
+    $('.add-form').click(function() {
+        $('form').trigger('reinitialize.areYouSure');
+      });
 });
