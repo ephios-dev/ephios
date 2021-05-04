87eb9d823bd399bbd7824d164cb963c6fbe3e59e
Julian B <jeriox@users.noreply.github.com>
2021-05-04 23:07:16 +0200

    fix tests

--- tests/core/test_workinghours.py
+++ tests/core/test_workinghours.py
@@ -1,3 +1,4 @@
+from django.template.defaultfilters import floatformat
 from django.urls import reverse
 
 
@@ -5,11 +6,11 @@ class TestWorkingHours:
     def test_profile_without_hours(self, django_app, volunteer):
         response = django_app.get(reverse("core:profile"), user=volunteer)
         assert response.status_code == 200
-        assert response.html.find(text="0.0 hours")
+        assert response.html.find(text=f"{floatformat(0, arg=2)} hours")
 
     def test_profile_with_hours(self, django_app, volunteer, workinghours):
         response = django_app.get(reverse("core:profile"), user=volunteer)
         assert response.html.find(text=workinghours[0].reason)
         assert response.html.find(text=workinghours[1].reason)
         total_hours = workinghours[0].hours + workinghours[1].hours
-        assert response.html.find(text=f"{total_hours:.1f} hours")
+        assert response.html.find(text=f"{floatformat(total_hours, arg=2)} hours")
