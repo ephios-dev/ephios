296a04f779dd3fdebe2663ab8359aeb12927a69c
Felix Rindt <felix@rindt.me>
2021-05-28 22:31:26 +0200

    move primary keys to new BigAutoField

--- ephios/settings.py
+++ ephios/settings.py
@@ -82,6 +82,8 @@ INSTALLED_APPS += PLUGINS
 
 INSTALLED_APPS += ["dynamic_preferences"]  # must come after our apps to collect preferences
 
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
