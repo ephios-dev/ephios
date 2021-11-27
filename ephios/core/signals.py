75688fbe66ce4c047d9f16152cb0e10d5d598db7
Julian B <jeriox@users.noreply.github.com>
2021-11-27 01:30:31 +0100

    lint

--- ephios/core/signals.py
+++ ephios/core/signals.py
@@ -117,7 +117,7 @@ will contain a list of event ids on which the action should be performed.
 
 register_event_action = PluginSignal()
 """
-This signal is sent out to get a list of actions that a user can perform on a single event. The actions are 
+This signal is sent out to get a list of actions that a user can perform on a single event. The actions are
 displayed in the dropdown menu on the event detail view. Each action is represented by a dict with the keys
 ``url``, ``label`` and ``icon``. The given url will be called with a ``pk`` parameter containing the id of the event.
 """
