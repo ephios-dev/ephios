e27906c2ce19aa24ec2c12abd3ee519c30aac71f
Felix Rindt <felix@rindt.me>
2022-05-25 21:28:53 +0200

    enable Token Auth

--- ephios/api/urls.py
+++ ephios/api/urls.py
@@ -2,22 +2,21 @@ from django.urls import include, path
 from rest_framework import routers
 
 # Serializers define the API representation.
+from rest_framework.schemas import get_schema_view
+
 from ephios.api.events import EventViewSet
+from django.views.generic import TemplateView
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r"events", EventViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+app_name = "api"
 urlpatterns = [
-    path(
-        "v<int:version>/",
-        include(
-            [
-                path("", include(router.urls)),
-                path("auth/", include("rest_framework.urls", namespace="rest_framework")),
-            ]
-        ),
-    )
+    path("", include(router.urls)),
+    path('schema/', get_schema_view(
+        title="ephios",
+        description="ephios API",
+        version="1.0.0"
+    ), name='openapi-schema'),
 ]
