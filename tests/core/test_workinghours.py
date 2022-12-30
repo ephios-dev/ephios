81c9420a4a25a202b99694ddb0bd6994e7d18b3c
Julian Baumann <jeriox@users.noreply.github.com>
2022-12-30 20:35:21 +0100

    fix mobile layout for workinghour views (#843)

--- tests/core/test_workinghours.py
+++ tests/core/test_workinghours.py
@@ -11,14 +11,14 @@ class TestWorkingHours:
     def test_own_workinghours_without_hours(self, django_app, volunteer):
         response = django_app.get(reverse("core:workinghours_own"), user=volunteer)
         assert response.status_code == 200
-        assert response.html.find(text=f"{floatformat(0, arg=2)} hours")
+        assert response.html.find(text=f"{floatformat(0, arg=2)}")
 
     def test_own_workinghours(self, django_app, volunteer, workinghours):
         response = django_app.get(reverse("core:workinghours_own"), user=volunteer)
         assert response.html.find(text=workinghours[0].reason)
         assert response.html.find(text=workinghours[1].reason)
         total_hours = workinghours[0].hours + workinghours[1].hours
-        assert response.html.find(text=f"{floatformat(total_hours, arg=2)} hours")
+        assert response.html.find(text=f"{floatformat(total_hours, arg=2)}")
 
     def test_workinghours_rounding(self, django_app, volunteer, event):
         from ephios.core.models import AbstractParticipation, LocalParticipation
