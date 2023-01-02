45918b28c416ae2ef803f5312ff6c1697b2514f1
Julian B <jeriox@users.noreply.github.com>
2023-01-02 22:29:45 +0100

    fix wrong media import for DjangoSelect2Widget

--- ephios/plugins/simpleresource/models.py
+++ ephios/plugins/simpleresource/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from ephios.core.models import Shift
 
 
 class ResourceCategory(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, verbose_name=_("Name"))
 
     def __str__(self):
         # pylint: disable=invalid-str-returned
@@ -12,8 +13,10 @@ class ResourceCategory(models.Model):
 
 
 class Resource(models.Model):
-    title = models.CharField(max_length=100)
-    category = models.ForeignKey(ResourceCategory, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, verbose_name=_("Title"))
+    category = models.ForeignKey(
+        ResourceCategory, on_delete=models.CASCADE, verbose_name=_("Category")
+    )
 
     def __str__(self):
         # pylint: disable=invalid-str-returned
@@ -22,7 +25,7 @@ class Resource(models.Model):
 
 class ResourceAllocation(models.Model):
     shift = models.ForeignKey(Shift, on_delete=models.CASCADE)
-    resources = models.ManyToManyField(Resource, blank=True)
+    resources = models.ManyToManyField(Resource, blank=True, verbose_name=_("Resources"))
 
     def __str__(self):
         return f"Resource allocation for {self.shift}"
