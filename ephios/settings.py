836c729cf5105bc110d0dec4393e38eebb51d1d7
Felix Rindt <felix@rindt.me>
2023-01-10 14:00:08 +0100

    add API pagination

--- ephios/settings.py
+++ ephios/settings.py
@@ -306,6 +306,8 @@ def GET_SITE_URL():
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.DjangoObjectPermissions"],
     "DEFAULT_VERSIONING_CLASS": "rest_framework.versioning.NamespaceVersioning",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 100,
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
