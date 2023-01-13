7bb494902db327fa79c56aa3a3713b42343015b8
Felix Rindt <felix@rindt.me>
2023-01-13 19:41:56 +0100

    disable dynamic preferences cache when testing

--- tests/settings.py
+++ tests/settings.py
@@ -1,3 +1,8 @@
 from ephios.settings import *  # no-qa
 
 LANGUAGE_CODE = "en"
+
+DYNAMIC_PREFERENCES = {
+    # the cache is not invalidated between tests, so tests aren't isolated properly with the cache enabled
+    "ENABLE_CACHE": False,
+}
