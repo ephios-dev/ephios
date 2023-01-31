7141e04b69a1ec9e3e3cb4f01d44fc120b58b400
Felix Rindt <felix@rindt.me>
2023-01-31 10:55:04 +0100

    fix deprecated bs4 kwarg

--- tests/plugins/simpleresource/test_resources.py
+++ tests/plugins/simpleresource/test_resources.py
@@ -7,8 +7,8 @@ from ephios.plugins.simpleresource.models import Resource, ResourceAllocation, R
 
 def test_resource_list(django_app, superuser, resources):
     response = django_app.get(reverse("simpleresource:resource_list"), user=superuser)
-    assert response.html.find(text=resources[0].title)
-    assert response.html.find(text=resources[1].title)
+    assert response.html.find(string=resources[0].title)
+    assert response.html.find(string=resources[1].title)
 
 
 def test_resource_edit(django_app, superuser, resources):
@@ -19,7 +19,7 @@ def test_resource_edit(django_app, superuser, resources):
     form["title"] = new_title
     response = form.submit().follow()
     assert response.status_code == 200
-    assert response.html.find(text=new_title)
+    assert response.html.find(string=new_title)
 
 
 def test_resource_delete(django_app, superuser, resources):
@@ -58,8 +58,8 @@ def test_resource_allocation_display(django_app, volunteer, groups, event, resou
     allocation = ResourceAllocation.objects.create(shift=event.shifts.first())
     allocation.resources.set([resources[0]])
     response = django_app.get(event.get_absolute_url(), user=volunteer)
-    assert response.html.find(text=re.compile(f"{resources[0].title}"))
-    assert not response.html.find(text=re.compile(f"{resources[1].title}"))
+    assert response.html.find(string=re.compile(f"{resources[0].title}"))
+    assert not response.html.find(string=re.compile(f"{resources[1].title}"))
 
 
 def test_resource_allocation_edit(django_app, superuser, groups, event, resources):
