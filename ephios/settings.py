d1d287e4bc80868423d3ef6d9193732faa0d8461
Julian B <jeriox@users.noreply.github.com>
2023-03-12 13:53:26 +0100

    remove DRF token auth

--- ephios/settings.py
+++ ephios/settings.py
@@ -51,7 +51,6 @@ INSTALLED_APPS = [
     "guardian",
     "oauth2_provider",
     "rest_framework",
-    "rest_framework.authtoken",
     "django_select2",
     "djangoformsetjs",
     "compressor",
@@ -310,7 +309,6 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 100,
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "oauth2_provider.contrib.rest_framework.OAuth2Authentication",
     ],
