bf4a3236c877206552cdea5a08999bc41e176f3a
Felix Rindt <felix@rindt.me>
2023-06-24 01:24:55 +0200

    configurable env file location

--- ephios/urls.py
+++ ephios/urls.py
@@ -7,7 +7,7 @@ Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
-    1. Add an import:  from other_app.views import Home
+    1. Add an import: from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
