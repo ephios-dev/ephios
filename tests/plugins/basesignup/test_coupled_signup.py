503db27022896469f1058207fc6600109c76f4d1
Felix Rindt <felix@rindt.me>
2023-07-15 21:57:20 +0200

    fix editing coupled signup shift with missing leader shift

--- tests/plugins/basesignup/test_coupled_signup.py
+++ tests/plugins/basesignup/test_coupled_signup.py
@@ -1,9 +1,13 @@
 from datetime import datetime
 
 import pytest
+from django.template.loader import get_template
+from django.urls import reverse
 
 from ephios.core.models import AbstractParticipation, LocalParticipation
+from ephios.modellogging.models import LogEntry
 from ephios.plugins.basesignup.signup.coupled_signup import CoupledSignupMethod
+from ephios.plugins.basesignup.signup.request_confirm import RequestConfirmSignupMethod
 
 
 @pytest.fixture
@@ -67,10 +71,35 @@ def test_event_detail_with_missing_leader_shift(django_app, volunteer, event, cr
     assert "leading shift is missing" in response
 
 
-def test_coupled_signupstats(django_app, volunteer, event, create_coupled_shift):
-    shift = event.shifts.first()
-    shift.signup_configuration["maximum_number_of_participants"] = 1
-    shift.save()
+def test_editing_coupled_shift_with_missing_leader_shift(
+    django_app, planner, event, create_coupled_shift, tz
+):
     coupled_shift = create_coupled_shift()
-    signup_stats = event.get_signup_stats()
-    assert signup_stats.free == 1
+    response = django_app.get(
+        reverse("core:shift_delete", kwargs={"pk": event.shifts.first().pk}), user=planner
+    )
+    response = response.form.submit().follow()
+    assert "leading shift is missing" in response
+    new_shift = event.shifts.create(
+        meeting_time=datetime(2099, 7, 1, 7, 0).astimezone(tz),
+        start_time=datetime(2099, 7, 1, 8, 0).astimezone(tz),
+        end_time=datetime(2099, 7, 1, 20, 0).astimezone(tz),
+        signup_method_slug=RequestConfirmSignupMethod.slug,
+    )
+    response = django_app.get(
+        reverse("core:shift_edit", kwargs={"pk": coupled_shift.pk}), user=planner
+    )
+    response.form["leader_shift_id"] = new_shift.pk
+    response.form.submit()
+    log_message = get_template("modellogging/logentry.html").render(
+        {"log": LogEntry.objects.first()}
+    )
+    assert "missing" in log_message
+
+    def test_coupled_signupstats(django_app, volunteer, event, create_coupled_shift):
+        shift = event.shifts.first()
+        shift.signup_configuration["maximum_number_of_participants"] = 1
+        shift.save()
+        coupled_shift = create_coupled_shift()
+        signup_stats = event.get_signup_stats()
+        assert signup_stats.free == 1
