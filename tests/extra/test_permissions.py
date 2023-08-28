53166af2a184a85e2985aee2f22915bfc40749aa
Felix Rindt <felix@rindt.me>
2023-08-28 08:56:08 +0200

    add test for demoting last management group

--- tests/extra/test_permissions.py
+++ tests/extra/test_permissions.py
@@ -1,3 +1,5 @@
+import pytest
+
 from ephios.core.forms.users import CORE_MANAGEMENT_PERMISSIONS
 from ephios.extra.permissions import get_groups_with_perms
 
@@ -22,3 +24,15 @@ def test_querying_get_groups_with_perms(groups, event):
         must_have_all_perms=True,
     )
     assert set(management_groups_by_list) == {managers}
+
+    management_groups_by_list = get_groups_with_perms(
+        only_with_perms_in=["core.view_event", "core.change_event"],
+        must_have_all_perms=False,
+    )
+    assert set(management_groups_by_list) == {managers}
+
+    with pytest.raises(ValueError):
+        # view_event is not a specific enough to identify a permission
+        get_groups_with_perms(
+            only_with_perms_in=["view_event"],
+        )
