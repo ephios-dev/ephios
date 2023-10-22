e0157f6e23a3e7e27d061d3242c4a41d55e8bb79
Felix Rindt <felix@rindt.me>
2023-10-22 23:28:46 +0200

    lint

--- ephios/plugins/complexsignup/signals.py
+++ ephios/plugins/complexsignup/signals.py
@@ -8,6 +8,4 @@ from ephios.core.signup.methods import register_signup_methods
     dispatch_uid="ephios.plugins.complexsignup.signals.register_complex_signup_method",
 )
 def register_complex_signup_method(sender, **kwargs):
-    return [
-        # TODO
-    ]
+    return []
