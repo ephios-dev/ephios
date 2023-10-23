c3a63dd92a6900f4472d0f3674d3d406452a6483
Julian B <jeriox@users.noreply.github.com>
2023-10-23 20:17:48 +0200

    add test for user oidc user creation

--- tests/core/test_auth.py
+++ tests/core/test_auth.py
@@ -27,6 +27,27 @@ def test_oidc_backend_authenticate(
     assert user == volunteer
 
 
+def test_oidc_backend_create_user(oidc_client, groups):
+    from ephios.extra.auth import EphiosOIDCAB
+
+    managers, planners, volunteers = groups
+    claims = {
+        "email": "test@localhost",
+        "name": "Test User",
+        "phone_number": "12345",
+        "birthdate": "2000-01-01",
+    }
+    oidc_client.default_groups.set([volunteers])
+    backend = EphiosOIDCAB()
+    backend.provider = oidc_client
+    user = backend.create_user(claims)
+    assert user.email == claims["email"]
+    assert user.display_name == claims["name"]
+    assert user.phone == claims["phone_number"]
+    assert user.date_of_birth.isoformat() == claims["birthdate"]
+    assert user.groups.count() == 1
+
+
 def test_oidc_initiate(django_app, oidc_client):
     response = django_app.get(reverse("core:oidc_initiate", kwargs={"provider": oidc_client.id}))
     assert response.status_code == 302
