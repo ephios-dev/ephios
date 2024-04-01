8ab0e52448a63ed3d2cabce2919186dfc175dcde
Julian B <jeriox@users.noreply.github.com>
2024-04-02 00:15:59 +0200

    correct handling of permissions on participation endpoint

---
+++ ephios/api/filters.py
@@ -0,0 +1,8 @@
+from guardian.shortcuts import get_objects_for_user
+from rest_framework.filters import BaseFilterBackend
+
+
+class ParticipationPermissionFilter(BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        events = get_objects_for_user(request.user, "core.view_event")
+        return queryset.filter(shift__event__in=events)
--- ephios/api/views/users.py
+++ ephios/api/views/users.py
@@ -15,6 +15,7 @@ from rest_framework.serializers import ModelSerializer
 from rest_framework.viewsets import GenericViewSet
 from rest_framework_guardian.filters import ObjectPermissionsFilter
 
+from ephios.api.filters import ParticipationPermissionFilter
 from ephios.api.views.events import ParticipationSerializer
 from ephios.core.models import LocalParticipation, Qualification, UserProfile
 from ephios.core.services.qualification import collect_all_included_qualifications
@@ -103,7 +104,7 @@ class UserByMailView(RetrieveModelMixin, GenericViewSet):
 class UserParticipationView(viewsets.ReadOnlyModelViewSet):
     serializer_class = ParticipationSerializer
     permission_classes = [IsAuthenticatedOrTokenHasScope]
-    filter_backends = [ObjectPermissionsFilter, DjangoFilterBackend]
+    filter_backends = [ParticipationPermissionFilter, DjangoFilterBackend]
     filterset_fields = ["state"]
     required_scopes = ["CONFIDENTIAL_READ"]
 
