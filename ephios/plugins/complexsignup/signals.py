3e35b8aea3f38fcc42c647a901683755cc7a61e8
Felix Rindt <felix@rindt.me>
2024-08-26 10:25:43 +0200

    add structure

---
+++ ephios/plugins/complexsignup/signals.py
@@ -0,0 +1,14 @@
+from django.dispatch import receiver
+
+from ephios.core.signals import register_shift_structures
+from ephios.plugins.complexsignup.signup import ComplexShiftStructure
+
+
+@receiver(
+    register_shift_structures,
+    dispatch_uid="ephios.plugins.complexsignup.signals.register_complex_shift_structures",
+)
+def register_complex_shift_structures(sender, **kwargs):
+    return [
+        ComplexShiftStructure,
+    ]
