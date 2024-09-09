0a5d109d6636822f40b4f02f7323897e7ae936e8
Julian B <jeriox@users.noreply.github.com>
2024-09-09 20:19:25 +0200

    add hour picking

--- ephios/static/ephios/js/event_copy.js
+++ ephios/static/ephios/js/event_copy.js
@@ -1,15 +1,24 @@
 const {createApp, ref, computed} = Vue
 
-function formatDate(date_obj, sep="-") {
+function formatDate(date_obj, sep = "-") {
     let month = date_obj.getMonth() + 1;
     let day = date_obj.getDate() + 1;
     return date_obj.getFullYear() + sep + (month < 10 ? "0" : "") + month + sep + (day < 10 ? "0" : "") + day
 }
 
+function formatHourOrZero(time_string) {
+    if (time_string) {
+        return pickHour ? time_string.slice(0,2) + time_string.slice(3,5) + "00" : "000000"
+    }
+    return "000000"
+}
+
 document.addEventListener('DOMContentLoaded', (event) => {
     createApp({
         setup() {
+            const pickHour = JSON.parse(document.getElementById("pickHour").value);
             const DTSTART = ref(formatDate(new Date()))
+            const DTSTART_TIME = ref("00:00")
             const rules = ref([])
             const dates = ref([])
             const weekdays = [{id: "MO", short: "Mon", long: "Monday"}, {
@@ -37,8 +46,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 rules.value.splice(rules.value.indexOf(rule), 1)
             }
 
-             async function addDate() {
-                dates.value.push({date: ""});
+            async function addDate() {
+                dates.value.push({date: "", time: ""});
             }
 
             async function removeDate(date) {
@@ -58,39 +67,58 @@ document.addEventListener('DOMContentLoaded', (event) => {
             })
 
             const rrule_string = computed(() => {
-                return "DTSTART;TZID=" + Intl.DateTimeFormat().resolvedOptions().timeZone + ":" + formatDate(new Date(DTSTART.value), "") + "T000000\n" + rules.value.map(rule => {
-                    let rule_str = "FREQ=" + rule.freq + ";INTERVAL=" + rule.interval;
-                    switch (rule.freq) {
-                        case "WEEKLY":
-                            rule_str += ";BYWEEKDAY=" + (Array.isArray(rule.BYWEEKDAY) ? rule.BYWEEKDAY.join(",") : rule.BYWEEKDAY);
-                            break;
-                        case "MONTHLY":
-                            if (rule.month_mode === "BYMONTHDAY") {
-                                rule_str += ";BYMONTHDAY=" + rule.BYMONTHDAY
-                            } else {
-                                rule_str += ";BYWEEKDAY=" + rule.BYWEEKDAY + ";BYSETPOS=" + rule.BYSETPOS
+                return "DTSTART;TZID=" + Intl.DateTimeFormat().resolvedOptions().timeZone + ":" +
+                formatDate(new Date(DTSTART.value), "") + "T" + formatHourOrZero(DTSTART_TIME.value) + "\n" +
+                    rules.value.map(rule => {
+                        let rule_str = "FREQ=" + rule.freq + ";INTERVAL=" + rule.interval;
+                        switch (rule.freq) {
+                            case "WEEKLY":
+                                rule_str += ";BYWEEKDAY=" + (Array.isArray(rule.BYWEEKDAY) ? rule.BYWEEKDAY.join(",") : rule.BYWEEKDAY);
+                                break;
+                            case "MONTHLY":
+                                if (rule.month_mode === "BYMONTHDAY") {
+                                    rule_str += ";BYMONTHDAY=" + rule.BYMONTHDAY
+                                } else {
+                                    rule_str += ";BYWEEKDAY=" + rule.BYWEEKDAY + ";BYSETPOS=" + rule.BYSETPOS
+                                }
+                                break;
+                            case "YEARLY":
+                                if (rule.year_mode === "BYMONTHDAY") {
+                                    rule_str += ";BYMONTHDAY=" + rule.BYMONTHDAY + ";BYMONTH=" + rule.BYMONTH
+                                } else {
+                                    rule_str += ";BYWEEKDAY=" + rule.BYWEEKDAY + ";BYSETPOS=" + rule.BYSETPOS + ";BYMONTH=" + rule.BYMONTH
+                                }
+                        }
+                        if (rule.end_mode === "COUNT") {
+                            rule_str += ";COUNT=" + rule.COUNT
+                        } else if (rule.end_mode === "UNTIL") {
+                            rule_str += ";UNTIL=" + formatDate(new Date(rule.UNTIL), "")
+                            if (pickHour) {
+                                rule_str += "T" + formatHourOrZero(rule.UNTIL_TIME)
                             }
-                            break;
-                        case "YEARLY":
-                            if (rule.year_mode === "BYMONTHDAY") {
-                                rule_str += ";BYMONTHDAY=" + rule.BYMONTHDAY + ";BYMONTH=" + rule.BYMONTH
-                            } else {
-                                rule_str += ";BYWEEKDAY=" + rule.BYWEEKDAY + ";BYSETPOS=" + rule.BYSETPOS + ";BYMONTH=" + rule.BYMONTH
-                            }
-                    }
-                    if (rule.end_mode === "COUNT") {
-                        rule_str += ";COUNT=" + rule.COUNT
-                    } else if (rule.end_mode === "UNTIL") {
-                        rule_str += ";UNTIL=" + rule.UNTIL
-                    }
-                    return rule_str;
-                }).join("\n") + dates.value.map(date => {
-                    return "RDATE;TZID=" + Intl.DateTimeFormat().resolvedOptions().timeZone + ":" + formatDate(new Date(date.date), "") + "T000000"
-                }).join("\n")
+                        }
+                        return rule_str;
+                    }).join("\n") + dates.value.map(date => {
+                        return "RDATE;TZID=" + Intl.DateTimeFormat().resolvedOptions().timeZone +
+                            ":" + formatDate(new Date(date.date), "") + "T" + formatHourOrZero(date.time)
+                    }).join("\n")
             })
 
             return {
-                rules, DTSTART, addRule, removeRule, addDate, removeDate, submitForm, rrule_string, weekdays, months, dates, computed_dates,
+                rules,
+                DTSTART,
+                DTSTART_TIME,
+                addRule,
+                removeRule,
+                addDate,
+                removeDate,
+                submitForm,
+                rrule_string,
+                weekdays,
+                months,
+                dates,
+                computed_dates,
+                pickHour,
             }
         }, delimiters: ['[[', ']]']
     }).mount('#recurrence');
