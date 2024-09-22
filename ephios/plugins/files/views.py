f1ee9bd1acdf547e6867f23e3057ab9fa576ae5a
Julian B <jeriox@users.noreply.github.com>
2024-09-22 13:58:50 +0200

    add fallback media serving

--- ephios/plugins/files/views.py
+++ ephios/plugins/files/views.py
@@ -2,7 +2,7 @@ import os
 from urllib.parse import urlsplit
 
 from django.conf import settings
-from django.http import HttpResponse
+from django.http import FileResponse, HttpResponse
 from django.shortcuts import get_object_or_404, redirect
 from django.views import View
 from guardian.mixins import LoginRequiredMixin
@@ -15,9 +15,12 @@ class DocumentView(LoginRequiredMixin, View):
         if (loc := urlsplit(settings.GET_USERCONTENT_URL()).netloc) and request.get_host() != loc:
             return redirect(settings.GET_USERCONTENT_URL() + request.path)
         document = get_object_or_404(Document, id=kwargs["pk"])
-        response = HttpResponse()
+        if settings.FALLBACK_MEDIA_SERVING:
+            response = FileResponse(document.file)
+        else:
+            response = HttpResponse()
+            response["X-Accel-Redirect"] = document.file.url
         response["Content-Disposition"] = (
             "attachment; filename=" + os.path.split(document.file.name)[1]
         )
-        response["X-Accel-Redirect"] = document.file.url
         return response
