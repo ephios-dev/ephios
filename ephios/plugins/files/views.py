ab56c341ddbfd99244680b646391188e182b960c
Julian B <jeriox@users.noreply.github.com>
2024-10-09 17:13:52 +0200

    refactor media file handling to middleware and mixin

--- ephios/plugins/files/views.py
+++ ephios/plugins/files/views.py
@@ -1,35 +1,20 @@
-import os
-from urllib.parse import urlsplit
-
-from django.conf import settings
 from django.contrib.messages.views import SuccessMessageMixin
-from django.http import FileResponse, HttpResponse
-from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
-from django.views import View
-from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 from guardian.mixins import LoginRequiredMixin
 
-from ephios.extra.mixins import CustomPermissionRequiredMixin
+from ephios.extra.mixins import CustomPermissionRequiredMixin, MediaViewMixin
+from ephios.extra.utils import accelerated_media_response
 from ephios.plugins.files.forms import DocumentForm
 from ephios.plugins.files.models import Document
 
 
-class DocumentView(LoginRequiredMixin, View):
+class DocumentView(LoginRequiredMixin, MediaViewMixin, DetailView):
+    model = Document
+
     def get(self, request, *args, **kwargs):
-        if (loc := urlsplit(settings.GET_USERCONTENT_URL()).netloc) and request.get_host() != loc:
-            return redirect(settings.GET_USERCONTENT_URL() + request.path)
-        document = get_object_or_404(Document, id=kwargs["pk"])
-        if settings.FALLBACK_MEDIA_SERVING:
-            response = FileResponse(document.file)
-        else:
-            response = HttpResponse()
-            response["X-Accel-Redirect"] = document.file.url
-        response["Content-Disposition"] = (
-            "attachment; filename=" + os.path.split(document.file.name)[1]
-        )
-        return response
+        return accelerated_media_response(self.get_object().file)
 
 
 class DocumentListView(CustomPermissionRequiredMixin, ListView):
