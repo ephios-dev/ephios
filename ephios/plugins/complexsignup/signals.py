979f2ece65af43b93e678e083a08bf4c9c1bbe47
Julian B <jeriox@users.noreply.github.com>
2024-10-25 11:38:01 +0200

    allow nav grouping

--- ephios/plugins/complexsignup/signals.py
+++ ephios/plugins/complexsignup/signals.py
@@ -1,6 +1,8 @@
 from django.dispatch import receiver
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
 
-from ephios.core.signals import register_shift_structures
+from ephios.core.signals import nav_link, register_shift_structures
 from ephios.plugins.complexsignup.structure import ComplexShiftStructure
 
 
@@ -12,3 +14,20 @@ def register_complex_shift_structures(sender, **kwargs):
     return [
         ComplexShiftStructure,
     ]
+
+
+@receiver(nav_link, dispatch_uid="ephios.plugins.complexsignup.signals.nav_link")
+def add_nav_link(sender, request, **kwargs):
+    return (
+        [
+            {
+                "label": _("Signup blocks"),
+                "url": reverse_lazy("complexsignup:blocks_editor"),
+                "active": request.resolver_match
+                and request.resolver_match.app_name == "complexsignup",
+                "group": _("Plugins"),
+            }
+        ]
+        if request.user.has_perm("core.delete_event")
+        else []
+    )
