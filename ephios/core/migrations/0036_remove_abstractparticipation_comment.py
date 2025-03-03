a30d7ba4bdb358dc9c354a238ba59bcf3a42454c
Julian B. <jeriox@users.noreply.github.com>
2025-03-03 20:59:22 +0100

    fix migrations

--- ephios/core/migrations/0036_remove_abstractparticipation_comment.py
+++ ephios/core/migrations/0036_remove_abstractparticipation_comment.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("core", "0034_participationcomment"),
+        ("core", "0035_participationcomment"),
     ]
 
     operations = [
