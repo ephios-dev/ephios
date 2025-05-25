9878b38250ae7e73bcd680974d5312be0a7f06a5
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2025-05-25 19:42:45 +0200

    Add question selection to shift form

--- ephios/plugins/questionnaires/forms.py
+++ ephios/plugins/questionnaires/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
+from django_select2.forms import Select2MultipleWidget
 
+from ephios.core.forms.events import BasePluginFormMixin
 from ephios.plugins.questionnaires.models import Question
 
 
@@ -31,3 +33,28 @@ class QuestionForm(forms.ModelForm):
             if "name" in choice and not choice["DELETE"]
         ]
         return super().save(commit)
+
+
+class QuestionnaireForm(BasePluginFormMixin, forms.Form):
+    questions = forms.ModelMultipleChoiceField(
+        queryset=Question.objects.all(), widget=Select2MultipleWidget, required=False
+    )
+
+    def __init__(self, *args, shift, **kwargs):
+        kwargs.setdefault("prefix", "questionnaires")
+        super().__init__(*args, **kwargs)
+        self.shift = shift
+        self.fields["questions"].initial = Question.objects.filter(pk__in=self.shift.questionnaire)
+
+    def save(self):
+        self.shift.questionnaire = [
+            question.pk for question in self.cleaned_data["questions"].all()
+        ]
+        self.shift.save()
+
+    @property
+    def heading(self):
+        return _("Questionnaire")
+
+    def is_function_active(self):
+        return bool(len(self.shift.questionnaire) > 0)
