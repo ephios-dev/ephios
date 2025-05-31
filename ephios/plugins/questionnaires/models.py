368c92c00a4b6cde7b5cd73ad18d0bcb7082430c
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2025-05-31 19:35:18 +0200

    Save answers to participation and user profile

--- ephios/plugins/questionnaires/models.py
+++ ephios/plugins/questionnaires/models.py
@@ -1,5 +1,12 @@
+from django import forms
 from django.db import models
+from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
+from rest_framework import serializers
+
+from ephios.core.models.events import AbstractParticipation
+from ephios.core.signup.forms import AdditionalField
+from ephios.core.signup.participants import AbstractParticipant, LocalUserParticipant
 
 
 class Question(models.Model):
@@ -39,3 +46,67 @@ class Question(models.Model):
     def __str__(self):
         # pylint: disable=invalid-str-returned
         return self.name
+
+    def get_signup_formfield(
+        self, participant: AbstractParticipant, participation: AbstractParticipation, signup_choice
+    ):
+        # Restore answer from participation (when editing) or the user's saved answers (if local participant)
+        answer_key = str(self.pk)
+        if answer_key in participation.questionnaire_answers:
+            initial = participation.questionnaire_answers[answer_key]
+        elif (
+            isinstance(participant, LocalUserParticipant)
+            and answer_key in participant.user.saved_questionnaire_answers
+        ):
+            initial = participant.user.saved_questionnaire_answers[answer_key]
+        else:
+            initial = None
+
+        # Reset answer if selected option does not exist
+        if self.type != self.TYPE_TEXT and initial not in self.choices:
+            initial = None
+
+        field_name = slugify(f"{self.pk} {self.name}")
+        required = self.required if initial is not None else False
+
+        form_class = (
+            forms.CharField
+            if self.type == self.TYPE_TEXT
+            else (
+                forms.MultipleChoiceField if self.type == self.TYPE_MULTIPLE else forms.ChoiceField
+            )
+        )
+        form_kwargs = {
+            "label": self.question_text,
+            "help_text": self.description,
+            "initial": initial,
+            "required": required,
+        }
+        if self.type == self.TYPE_SINGLE_RADIO:
+            form_kwargs["widget"] = forms.RadioSelect
+
+        serializer_class = (
+            serializers.CharField
+            if self.type == self.TYPE_TEXT
+            else (
+                serializers.MultipleChoiceField
+                if self.type == self.TYPE_MULTIPLE
+                else serializers.ChoiceField
+            )
+        )
+        serializer_kwargs = {
+            "required": required,
+        }
+
+        if self.type != self.TYPE_TEXT:
+            assert isinstance(
+                self.choices, list
+            ), f"The choices of question {self.name} are not a list"
+            # pylint: disable=not-an-iterable
+            choices = [(choice, choice) for choice in self.choices]
+            form_kwargs["choices"] = choices
+            serializer_kwargs["choices"] = choices
+
+        return AdditionalField(
+            field_name, form_class, form_kwargs, serializer_class, serializer_kwargs
+        )
