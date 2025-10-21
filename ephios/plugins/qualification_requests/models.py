1181fc38c76c6a75674b70a846cbc7f397305405
samuelson-ben <ben.samuelson@fiteka.de>
2025-10-21 20:41:02 +0200

    refactoring model and adding __str__ method

--- ephios/plugins/qualification_requests/models.py
+++ ephios/plugins/qualification_requests/models.py
@@ -1,40 +1,46 @@
 from django.db import models
+from django.db.models import (
+    CharField,
+    DateField,
+    DateTimeField,
+    ForeignKey,
+)
 from django.utils.translation import gettext_lazy as _
 from ephios.core.models import UserProfile, Qualification
 
 class QualificationRequest(models.Model):
-    user = models.ForeignKey(
+    user = ForeignKey(
         UserProfile,
         on_delete=models.CASCADE,
         related_name='qualification_requests',
         verbose_name=_("User"),
     )
-    qualification = models.ForeignKey(
+    qualification = ForeignKey(
         Qualification,
         on_delete=models.CASCADE,
         related_name='qualification_request',
         verbose_name=_("Qualification"),
     )
-    qualification_date = models.DateField(
+    qualification_date = DateField(
         null=False,
         blank=False,
         verbose_name=_("Qualification Date"),
     )
-    expiration_date = models.DateField(
+    expiration_date = DateField(
         null=True,
         blank=True,
         verbose_name=_("Expiration Date"),
     )
-    created_at = models.DateTimeField(
+    created_at = DateTimeField(
         auto_now_add=True,
         verbose_name=_("Created At"),
     )
-    user_comment = models.TextField(
+    user_comment = CharField(
         null=True,
         blank=True,
         verbose_name=_("User Comment"),
     )
-    status = models.CharField(
+    status = CharField(
         max_length=20,
         choices=[
             ('pending', _("Pending")),
@@ -44,8 +50,20 @@ class QualificationRequest(models.Model):
         default='pending',
         verbose_name=_("Status"),
     )
+    reason = CharField(
+        null=True,
+        blank=True,
+        verbose_name=_("Reason"),
+    )
     #image_data = models.BinaryField(null=True, blank=True)
     #image_content_type = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
-        return f"{self.user} requested {self.qualification} on {self.created_at}"
+        return _(
+            "%(user)s requested %(qualification)s on %(created)s (Status: %(status)s)"
+        ) % {
+            "user": self.user,
+            "qualification": self.qualification,
+            "created": self.created_at.strftime("%d.%m.%Y %H:%M"),
+            "status": self.status,  # zeigt die übersetzte Status-Option
+        }
