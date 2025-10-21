de3558fee28d28a8c50f2adb8e2b8279f163ff53
samuelson-ben <ben.samuelson@fiteka.de>
2025-10-21 20:42:39 +0200

    restructured add view and adding check and delete views

--- ephios/plugins/qualification_requests/views.py
+++ ephios/plugins/qualification_requests/views.py
@@ -1,17 +1,26 @@
 from django import forms
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import Group
 from django.db.models import Q, QuerySet
+from django.http import (
+    HttpResponseRedirect,
+    HttpResponseForbidden,
+)
 from django.urls import reverse_lazy
 from django.utils.functional import cached_property
 from django.utils.translation import gettext_lazy as _
-from django.views.generic import ListView, FormView
+from django.views.generic import (
+    ListView,
+    FormView,
+    DeleteView,
+)
 from django_select2.forms import ModelSelect2Widget
 
-from ephios.core.models import Qualification
+from ephios.core.models import Qualification, QualificationGrant
 from ephios.extra.mixins import CustomPermissionRequiredMixin
 from ephios.plugins.qualification_requests.forms import (
-    QualificationRequestAddForm,
+    QualificationRequestCreateForm,
     QualificationRequestCheckForm,
 )
 from ephios.plugins.qualification_requests.models import QualificationRequest
@@ -92,32 +101,26 @@ class QualificationRequestOwnListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         return QualificationRequest.objects.filter(user=self.request.user).order_by("-created_at")
 
-class QualificationRequestAddView(LoginRequiredMixin, FormView):
+class QualificationRequestOwnCreateView(LoginRequiredMixin, FormView):
     model = QualificationRequest
-    form_class = QualificationRequestAddForm
-    template_name = "qualification_requests/qualification_requests_form.html"
+    form_class = QualificationRequestCreateForm
+    template_name = "qualification_requests/qualification_requests_add_form.html"
     success_url = reverse_lazy("qualification_requests:qualification_requests_list_own")
 
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs.update({"initial": {"user": self.request.user}})
-        return kwargs
-
     def form_valid(self, form):
-        qualification_request = QualificationRequest.objects.create(
+        QualificationRequest.objects.create(
             user=self.request.user,
             qualification=form.instance.qualification,
-            qualification_date=form.instance.qualification_date
+            qualification_date=form.instance.qualification_date,
+            user_comment=form.instance.user_comment,
         )
-
-        print(f"Created qualification request: {qualification_request}")
 
         return super().form_valid(form)
 
-class QualificationRequestCheckView(LoginRequiredMixin, FormView):
+class QualificationRequestOwnUpdateView(LoginRequiredMixin, FormView):
     model = QualificationRequest
-    form_class = QualificationRequestCheckForm
-    template_name = "qualification_requests/qualification_requests_form.html"
+    form_class = QualificationRequestCreateForm
+    template_name = "qualification_requests/qualification_requests_update_form.html"
     success_url = reverse_lazy("qualification_requests:qualification_requests_list_own")
 
     def dispatch(self, request, *args, **kwargs):
@@ -128,14 +131,124 @@ class QualificationRequestCheckView(LoginRequiredMixin, FormView):
 
     def get_object(self):
         return QualificationRequest.objects.get(pk=self.kwargs["pk"])
-
+    
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
         kwargs.update({"instance": self.object})
         return kwargs
 
     def form_valid(self, form):
+        if self.object.status != "pending":
+            messages.error(
+                self.request,
+                _("You cannot edit a qualification request that is not pending.")
+            )
+            return self.form_invalid(form)
+        
         self.object.qualification = form.instance.qualification
         self.object.qualification_date = form.instance.qualification_date
+        self.object.user_comment = form.instance.user_comment
         self.object.save()
         return super().form_valid(form)
+
+class QualificationRequestCheckView(CustomPermissionRequiredMixin, FormView):
+    model = QualificationRequest
+    form_class = QualificationRequestCheckForm
+    template_name = "qualification_requests/qualification_requests_check_form.html"
+    success_url = reverse_lazy("qualification_requests:qualification_requests_list")
+    permission_required = "core.change_userprofile"
+
+    def dispatch(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_object(self):
+        return QualificationRequest.objects.get(pk=self.kwargs["pk"])
+    
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs.update({"instance": self.object})
+        return kwargs
+
+    def form_valid(self, form):
+        if self.object.status != "pending":
+            messages.error(
+                self.request,
+                _("You cannot edit a qualification request that is not pending.")
+            )
+            return self.form_invalid(form)
+        
+        self.object.qualification = form.instance.qualification
+        self.object.qualification_date = form.instance.qualification_date
+        self.object.expiration_date = form.instance.expiration_date
+        self.object.reason = form.instance.reason
+        self.object.save()
+
+        action = self.request.POST.get("action")
+        if action == "approve":
+            form.instance.status = "approved"
+            form.instance.save()
+            messages.success(self.request, _("Qualification request approved."))
+            self.grant_qualification()
+        elif action == "reject":
+            form.instance.status = "rejected"
+            form.instance.save()
+            messages.success(self.request, _("Qualification request rejected."))
+        return super().form_valid(form)
+    
+    def grant_qualification(self):
+        """Grant the qualification to the user if the request is approved."""
+        if self.object.status == "approved":
+            return QualificationGrant.objects.get_or_create(
+                user=self.object.user,
+                qualification=self.object.qualification,
+                expires=self.object.expiration_date if self.object.expiration_date else None,
+            )
+
+class QualificationRequestOwnDeleteView(LoginRequiredMixin, DeleteView):
+    model = QualificationRequest
+    template_name = "qualification_requests/qualification_requests_delete_form.html"
+    success_url = reverse_lazy("qualification_requests:qualification_requests_list_own")
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+
+        if self.object.user != request.user:
+            return HttpResponseForbidden(_("You have no rights to delete this request."))
+
+        if self.object.status == "pending":
+            messages.error(
+                self.request,
+                _("You cannot delete a qualification request that is pending.")
+            )
+            return HttpResponseRedirect(self.success_url)
+        
+        self.object.delete()
+        messages.success(
+            request,
+            _("Qualification request deleted.")
+        )
+        return HttpResponseRedirect(self.success_url)
+
+class QualificationRequestDeleteView(CustomPermissionRequiredMixin, DeleteView):
+    model = QualificationRequest
+    template_name = "qualification_requests/qualification_requests_delete_form.html"
+    success_url = reverse_lazy("qualification_requests:qualification_requests_list")
+    permission_required = "core.change_userprofile"
+    
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+
+        if self.object.status == "pending":
+            messages.error(
+                self.request,
+                _("You cannot delete a qualification request that is pending.")
+            )
+            return HttpResponseRedirect(self.success_url)
+        
+        self.object.delete()
+        messages.success(
+            request,
+            _("Qualification request deleted.")
+        )
+        return HttpResponseRedirect(self.success_url)
